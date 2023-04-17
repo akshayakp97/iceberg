7c5928270df78641df3ed094d16eadc5dbe706ac
Atreya Srivathsan <atreyas@amazon.com>
2023-04-17 11:20:26 -0700

    CherryPick; CherryPick: Add support for S3 CRT Client, fix nessie in version.props

--- aws/src/test/java/org/apache/iceberg/aws/TestAwsClientFactories.java
+++ aws/src/test/java/org/apache/iceberg/aws/TestAwsClientFactories.java
@@ -294,7 +294,7 @@ public class TestAwsClientFactories {
     }
 
     @Override
-    public S3AsyncClient s3Async() {
+    public S3AsyncClient s3Async(boolean useCrt) {
       return null;
     }
 
