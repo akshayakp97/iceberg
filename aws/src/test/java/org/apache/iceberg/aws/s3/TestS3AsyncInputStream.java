c32bc2a218cbf00d4e936313898862d82546cf82
Akshaya KP <apatta@amazon.com>
2023-05-17 11:45:11 -0700

    fix failing unit tests for async client

--- aws/src/test/java/org/apache/iceberg/aws/s3/TestS3AsyncInputStream.java
+++ aws/src/test/java/org/apache/iceberg/aws/s3/TestS3AsyncInputStream.java
@@ -20,35 +20,31 @@ package org.apache.iceberg.aws.s3;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static software.amazon.awssdk.http.SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES;
 
 import com.adobe.testing.s3mock.junit4.S3MockRule;
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
-
+import java.util.concurrent.CompletionException;
 import org.apache.commons.io.IOUtils;
+import org.apache.iceberg.io.RangeReadable;
 import org.apache.iceberg.io.SeekableInputStream;
-import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
-import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.BucketAlreadyExistsException;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.utils.AttributeMap;
 
 public class TestS3AsyncInputStream {
@@ -71,37 +67,6 @@ public class TestS3AsyncInputStream {
                         AttributeMap.builder().put(TRUST_ALL_CERTIFICATES, Boolean.TRUE).build()))
             .build();
     createBucket("bucket");
-  }
-
-  @Test
-  public void shouldUploadAsyncTest() {
-
-    CompletableFuture<ListBucketsResponse> listBuckets = s3.listBuckets();
-    Assertions.assertThat(listBuckets).isNotNull();
-
-    CompletableFuture<PutObjectResponse> future = s3.putObject(
-            PutObjectRequest.builder()
-                    .bucket("bucket")
-                    .key("key")
-                    .build(),
-            AsyncRequestBody.fromFile(Paths.get("file"))
-    );
-
-    future.whenComplete((resp, err) -> {
-      try {
-        if (resp != null) {
-          System.out.println("my response: " + resp);
-        } else {
-          // Handle error
-          err.printStackTrace();
-        }
-      } finally {
-        // Lets the application shut down. Only close the client when you are completely done with it.
-        s3.close();
-      }
-    });
-
-    future.join();
   }
 
   @Test
@@ -162,70 +127,71 @@ public class TestS3AsyncInputStream {
     assertArrayEquals(Arrays.copyOfRange(original, (int) rangeStart, (int) rangeEnd), actual);
   }
 
-  //    @Test
-  //    public void testRangeRead() throws Exception {
-  //        S3URI uri = new S3URI("s3://bucket/path/to/range-read.dat");
-  //        int dataSize = 1024 * 1024 * 10;
-  //        byte[] expected = randomData(dataSize);
-  //        byte[] actual = new byte[dataSize];
-  //
-  //        long position;
-  //        int offset;
-  //        int length;
-  //
-  //
-  //        try (RangeReadable in = new S3AsyncInputStream(s3, uri)) {
-  //            // first 1k
-  //            position = 0;
-  //            offset = 0;
-  //            length = 1024;
-  //            readAndCheckRanges(in, expected, position, actual, offset, length);
-  //
-  //            // last 1k
-  //            position = dataSize - 1024;
-  //            offset = dataSize - 1024;
-  //            readAndCheckRanges(in, expected, position, actual, offset, length);
-  //
-  //            // middle 2k
-  //            position = dataSize / 2 - 1024;
-  //            offset = dataSize / 2 - 1024;
-  //            length = 1024 * 2;
-  //            readAndCheckRanges(in, expected, position, actual, offset, length);
-  //        }
-  //    }
-  //
-  //    private void readAndCheckRanges(
-  //            RangeReadable in, byte[] original, long position, byte[] buffer, int offset, int
-  // length)
-  //            throws IOException {
-  //        in.readFully(position, buffer, offset, length);
-  //
-  //        assertArrayEquals(
-  //                Arrays.copyOfRange(original, offset, offset + length),
-  //                Arrays.copyOfRange(buffer, offset, offset + length));
-  //    }
-  //
-  //    @Test
-  //    public void testClose() throws Exception {
-  //        S3URI uri = new S3URI("s3://bucket/path/to/closed.dat");
-  //        SeekableInputStream closed = new S3AsyncInputStream(s3, uri);
-  //        closed.close();
-  //        assertThrows(IllegalStateException.class, () -> closed.seek(0));
-  //    }
-  //
-  //    @Test
-  //    public void testSeek() throws Exception {
-  //        S3URI uri = new S3URI("s3://bucket/path/to/seek.dat");
-  //        byte[] expected = randomData(1024 * 1024);
-  //
-  //        try (SeekableInputStream in = new S3AsyncInputStream(s3, uri)) {
-  //            in.seek(expected.length / 2);
-  //            byte[] actual = IOUtils.readFully(in, expected.length / 2);
-  //            assertArrayEquals(Arrays.copyOfRange(expected, expected.length / 2,
-  // expected.length), actual);
-  //        }
-  //    }
-  //
+  @Test
+  public void testRangeRead() throws Exception {
+    S3URI uri = new S3URI("s3://bucket/path/to/range-read.dat");
+    int dataSize = 1024 * 1024 * 10;
+    byte[] expected = randomData(dataSize);
+    byte[] actual = new byte[dataSize];
+
+    long position;
+    int offset;
+    int length;
+
+    writeS3Data(uri, expected);
+
+    try (RangeReadable in = new S3AsyncInputStream(s3, uri)) {
+      // first 1k
+      position = 0;
+      offset = 0;
+      length = 1024;
+      readAndCheckRanges(in, expected, position, actual, offset, length);
+
+      // last 1k
+      position = dataSize - 1024;
+      offset = dataSize - 1024;
+      readAndCheckRanges(in, expected, position, actual, offset, length);
+
+      // middle 2k
+      position = dataSize / 2 - 1024;
+      offset = dataSize / 2 - 1024;
+      length = 1024 * 2;
+      readAndCheckRanges(in, expected, position, actual, offset, length);
+    }
+  }
+
+  private void readAndCheckRanges(
+      RangeReadable in, byte[] original, long position, byte[] buffer, int offset, int length)
+      throws IOException {
+    in.readFully(position, buffer, offset, length);
+
+    assertArrayEquals(
+        Arrays.copyOfRange(original, offset, offset + length),
+        Arrays.copyOfRange(buffer, offset, offset + length));
+  }
+
+  @Test
+  public void testClose() throws Exception {
+    S3URI uri = new S3URI("s3://bucket/path/to/closed.dat");
+    SeekableInputStream closed = new S3AsyncInputStream(s3, uri);
+    closed.close();
+    assertThrows(IllegalStateException.class, () -> closed.seek(0));
+  }
+
+  @Test
+  public void testSeek() throws Exception {
+    S3URI uri = new S3URI("s3://bucket/path/to/seek.dat");
+    byte[] expected = randomData(1024 * 1024);
+
+    writeS3Data(uri, expected);
+
+    try (SeekableInputStream in = new S3AsyncInputStream(s3, uri)) {
+      in.seek(expected.length / 2);
+      byte[] actual = IOUtils.readFully(in, expected.length / 2);
+      assertArrayEquals(Arrays.copyOfRange(expected, expected.length / 2, expected.length), actual);
+    }
+  }
+
   private byte[] randomData(int size) {
     byte[] data = new byte[size];
     random.nextBytes(data);
@@ -234,19 +200,24 @@ public class TestS3AsyncInputStream {
 
   private void createBucket(String bucketName) {
     try {
-      s3.createBucket(CreateBucketRequest.builder().bucket(bucketName).build());
-    } catch (BucketAlreadyExistsException e) {
-      // don't do anything
+      s3.createBucket(CreateBucketRequest.builder().bucket(bucketName).build()).join();
+    } catch (CompletionException e) {
+      if (e.getCause().getClass().equals(BucketAlreadyExistsException.class)) {
+        // do nothing
+        return;
+      }
+      throw e;
     }
   }
 
   private void writeS3Data(S3URI uri, byte[] data) throws IOException {
     s3.putObject(
             PutObjectRequest.builder()
-                    .bucket(uri.bucket())
-                    .key(uri.key())
-                    .contentLength((long) data.length)
-                    .build(),
-            AsyncRequestBody.fromBytes(data)).join();
+                .bucket(uri.bucket())
+                .key(uri.key())
+                .contentLength((long) data.length)
+                .build(),
+            AsyncRequestBody.fromBytes(data))
+        .join();
   }
 }
